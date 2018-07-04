788610051599cbc647a71b6d4bb63ef45bdfa34a
Bendyug Dmitry <mitriy.b@gmail.com>
2018-07-04 17:46:32 +0300

    Сalculation of the average score is added. Have problems with outputting

--- ege_stats/EgeStats.java
+++ ege_stats/EgeStats.java
@@ -7,13 +7,89 @@ import java.net.URL;
 
 public class EgeStats {
     public static void main(String[] args) throws IOException {
-        URL url = new URL("http://cpk.msu.ru/rating/dep_01");
+        URL url = new URL("https://cabinet.spbu.ru/Lists/1k_EntryLists/list_900f0ad3-62a3-42b8-916f-baab8808930a.html#0fa5f7cb-69c6-41e9-ab92-a377b4ce6c81");
         BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
 
         String inputLine;
-        while ((inputLine = reader.readLine()) != null)
-            System.out.println(inputLine);
-        reader.close();
+        int kcp = 0;
+        int count = 0;
+        int math = 0;
+        int countMath = 0;
+        int inform = 0;
+        int countInform = 0;
+        int countRus = 0;
+        int rus = 0;
+        while (!(inputLine = reader.readLine()).equals("<tbody>"))
+            ;
 
+        while (inputLine != null){
+        for (int i = 0; i < 19; i++) {
+            inputLine = reader.readLine();
+            if (i == 9) { //Math
+                String mathString = inputLine;
+
+                try {
+                    mathString = mathString.replaceAll(" ", "");
+                    mathString = mathString.replaceAll("<td>", "");
+                    mathString = mathString.replaceAll("</td>", "");
+                    Double mathDouble = Double.parseDouble(mathString);
+                    int mathInt = mathDouble.intValue();
+                    math = math + mathInt;
+                    countMath++;
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+            }
+
+            if (i == 10) { //Informatics
+                String informString = inputLine;
+
+                try {
+                    informString = informString.replaceAll(" ", "");
+                    informString = informString.replaceAll("<td>", "");
+                    informString = informString.replaceAll("</td>", "");
+                    Double informDouble = Double.parseDouble(informString);
+                    int informInt = informDouble.intValue();
+                    inform = inform + informInt;
+                    countInform++;
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+            }
+
+            if (i == 11) { //Rus
+                String rusString = inputLine;
+
+                try {
+                    rusString = rusString.replaceAll(" ", "");
+                    rusString = rusString.replaceAll("<td>", "");
+                    rusString = rusString.replaceAll("</td>", "");
+                    Double rusDouble = Double.parseDouble(rusString);
+                    int rusInt = rusDouble.intValue();
+                    rus = rus + rusInt;
+                    countRus++;
+                }
+                catch (Exception e) {
+                    System.out.println(e);
+                }
+            }
+        }
+            double averMath = (double) math/countMath;
+            double averInform = (double) inform/countInform;
+            double averRus = (double) rus/countRus;
+            printAverageMath(averMath);
+            printAverageInform(averInform);
+            printAverageRus(averRus);
+        }
+        reader.close();
+    }
+    public static void printAverageMath (double averageMath) {
+        System.out.println(averageMath);
+    }
+    public static void printAverageInform (double averageInform) {
+        System.out.println(averageInform);
+    }
+    public static void printAverageRus (double averageRus) {
+        System.out.println(averageRus);
     }
 }
